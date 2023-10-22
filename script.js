6cc08e4c163d84b5a4fded9d91fc06514eafc266
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-10-22 17:29:49 -0300

    finishing project

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const containerVideos = document.querySelector(".videos__container");
 
 
-async function seekShow() {
+async function searchAndShowVideos() {
     try {
         const searching = await fetch("http://localhost:3000/videos");
         const videos = await searching.json();
 
         videos.forEach((video) => {
             if (video.category == "") {
-                throw new Erro ('Vídeo não tem categoria');
+                throw new Erro('Vídeo não tem categoria');
             }
             containerVideos.innerHTML += `
             <li class="videos__item">
@@ -17,6 +17,7 @@ async function seekShow() {
                     <img class="img-chanel" src="${video.image} alt="Logo do Canal">
                     <h3 class="title-video">${video.title}</h3>
                     <p class="title-chanel">${video.description}</p>
+                    <p class="category" hidden>${video.category}</p>
                 </div>
             </li>
             `;
@@ -28,4 +29,49 @@ async function seekShow() {
     }
 }
 
-seekShow();
+searchAndShowVideos();
+
+const barResearch = document.querySelector(".search__input");
+
+barResearch.addEventListener("input", filterSearch);
+
+function filterSearch(){
+    const videos = document.querySelectorAll(".videos__item");
+
+    if(barResearch.value != ""){
+        for(let video of videos){
+            let title = video.querySelector(".title-video").textContent.toLowerCase();
+            let valueFilter = barResearch.value.toLowerCase();
+
+            if(!title.includes(valueFilter)){
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+
+        }
+    } else {
+        videos.forEach(video => video.style.display = 'block');
+}
+}
+
+const buttonCategory = document.querySelectorAll(".higher__item");
+
+buttonCategory.forEach((botao) => {
+    let nameCategory = botao.getAttribute("name");
+    botao.addEventListener("click", () => filterCategory(nameCategory));
+})
+
+function filterCategory(filter){
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos){
+        let category = video.querySelector(".category").textContent.toLowerCase();
+        let valueFilter = filter.toLowerCase();
+
+        if(!category.includes(valueFilter) && valueFilter != 'tudo'){
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
