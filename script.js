ba93d9ba20be706c10ff0ec0e18cf1f58a220498
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-10-22 16:36:28 -0300

    added async/await and try...catch

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos) =>
-    videos.forEach((video)=> {
-        containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title="${video.title}" frameborder="0" allowfullscreen></iframe>
-            <div class="description-video">
-                <img class="img-chanel" src="${video.image} alt="Logo do Canal">
-                <h3 class="title-video">${video.title}</h3>
-                <p class="title-chanel">${video.description}</p>
-            </div>
-        </li>
-        `;
-    })
-)
-.catch((error) => {
-    containerVideos.innerHTML `<p> Houve um erro ao carregar os videos: ${error} </p>`;
-})
+
+async function seekShow() {
+    try {
+        const searching = await fetch("http://localhost:3000/videos");
+        const videos = await searching.json();
+
+        videos.forEach((video) => {
+            if (video.category == "") {
+                throw new Erro ('Vídeo não tem categoria');
+            }
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${video.url}" title="${video.title}" frameborder="0" allowfullscreen></iframe>
+                <div class="description-video">
+                    <img class="img-chanel" src="${video.image} alt="Logo do Canal">
+                    <h3 class="title-video">${video.title}</h3>
+                    <p class="title-chanel">${video.description}</p>
+                </div>
+            </li>
+            `;
+        });
+    } catch (error) {
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: $[error]</p>`
+    } finally {
+        alert('Isso sempre acontece')
+    }
+}
+
+seekShow();
